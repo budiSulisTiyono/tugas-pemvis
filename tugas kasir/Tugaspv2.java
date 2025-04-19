30f0e8a132a39d6666e53b5c19c4a49d4b7c4480
budiSulisTiyono <budytoretto21@gmail.com>
2025-04-19 13:02:00 +0700

    first

---
+++ tugas kasir/Tugaspv2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugaspv2;
+
+/**
+ *
+ * @author buddy
+ */
+public class Tugaspv2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
